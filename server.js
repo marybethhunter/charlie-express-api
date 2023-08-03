592a0d174da5df3889134b380e7fcb266a9bcd2e
Mary Beth Hunter <86667443+marybethhunter@users.noreply.github.com>
2023-08-03 12:43:37 -0500

    remove commented out code

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const { auth } = require("express-oauth2-jwt-bearer");
 const cors = require("cors");
-require("dotenv").config();
 const { expressjwt: jwt } = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 const axios = require("axios").default;
@@ -14,7 +13,7 @@ const clientSecret = process.env.AUTH0_CLIENT_SECRET;
 const managementAPIaudience = process.env.AUTH0_MGMT_AUDIENCE;
 
 const corsOptions = {
-  origin: "https://charlie-api-d3e4b8d942b1.herokuapp.com/",
+  origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
@@ -133,111 +132,6 @@ app.get("/api/private", checkJwt, async (req, res) => {
 
   res.json(finalList);
 });
-
-// app.get(
-//   "/api/private-scoped",
-//   checkJwt,
-//   // requiredScopes("read:actions read:clients read:triggers"),
-//   async (req, res) => {
-//     let options = {
-//       method: "POST",
-//       url: `${issuerBaseUrl}/oauth/token`,
-//       headers: { "content-type": "application/json" },
-//       data: {
-//         grant_type: "client_credentials",
-//         client_id: clientID,
-//         client_secret: clientSecret,
-//         audience: managementAPIaudience,
-//       },
-//     };
-
-//     const managementAPIToken = await axios.request(options).then((res) => {
-//       return `Bearer ${res.data.access_token}`;
-//     });
-
-//     const getAllActions = await axios
-//       .get(`${managementAPIaudience}actions/actions`, {
-//         headers: { authorization: managementAPIToken },
-//       })
-//       .then((res) => {
-//         return res.data;
-//       });
-
-//     const getAllClients = await axios
-//       .get(`${managementAPIaudience}clients`, {
-//         headers: { authorization: managementAPIToken },
-//       })
-//       .then((res) => {
-//         return res.data;
-//       });
-
-//     let finalList = [];
-//     let noActionMessage = {
-//       message: "This application has no associated actions.",
-//     };
-
-//     const match = (clients, actions) => {
-//       clients.forEach((client) => {
-//         const oneSingleClient = {
-//           name: client.name,
-//           id: client.client_id,
-//           actions: [],
-//         };
-//         finalList.push(oneSingleClient);
-//       });
-
-//       const actionsArray = Object.values(actions);
-
-//       const finalActionsArray = actionsArray.flat();
-
-//       finalList.forEach((client) => {
-//         const matchingActions = finalActionsArray.filter((action) =>
-//           action.code?.includes(client.id)
-//         );
-//         if (
-//           matchingActions.length > 0 &&
-//           req.auth.permissions.includes("read:triggers")
-//         ) {
-//           matchingActions.forEach((action) => {
-//             client.actions.push({
-//               id: action.id,
-//               name: action.name,
-//               trigger: action.supported_triggers[0].id,
-//             });
-//           });
-//         }
-//         if (
-//           matchingActions.length > 0 &&
-//           !req.auth.permissions.includes("read:triggers")
-//         ) {
-//           matchingActions.forEach((action) => {
-//             client.actions.push({
-//               id: action.id,
-//               name: action.name,
-//             });
-//           });
-//         } else {
-//           client.actions.push(noActionMessage);
-//         }
-//       });
-//     };
-
-//     match(getAllClients, getAllActions);
-
-//     const removeLast = (array) => {
-//       array.length--;
-//       return array;
-//     };
-
-//     removeLast(finalList);
-
-//     res.json(finalList);
-
-//     // res.json({
-//     //   message:
-//     //     "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
-//     // });
-//   }
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
